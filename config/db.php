d964b7441604695ca49bb4c72b0cfb0e9620dfe2
projectdanz <akunprojectdanz@gmail.com>
2025-06-22 14:46:01 +0700

    fix: fixes on $conn

--- config/db.php
+++ config/db.php
@@ -1,11 +1,11 @@
 <?php
 
 $host = "localhost";
-$db = "task_sb";
 $user = "masdanz";
 $pass = "masdanz";
+$db = "task_sb";
 
-$conn = new mysqli($host, $db, $user, $pass);
+$conn = new mysqli($host, $user, $pass, $db);
 
 if ($conn->connect_error) {
     die('Connection failed: ' . $conn->connect_error);
