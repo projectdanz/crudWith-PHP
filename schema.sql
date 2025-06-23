86ddc482f2effb2f63788f412b1fd64137cf072f
projectdanz <akunprojectdanz@gmail.com>
2025-06-23 09:20:21 +0700

    fix: benerin beberapa typo

--- schema.sql
+++ schema.sql
@@ -4,12 +4,12 @@ CREATE TABLE users (
     password VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE task (
+CREATE TABLE tasks (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
     title VARCHAR(255) NOT NULL,
     description TEXT,
     is_active TINYINT(1) DEFAULT 1,
-    cretaed_at DATETIME CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFRENCES users(id)
+    creted_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
